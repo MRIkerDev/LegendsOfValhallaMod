0bd9dcb4918318267d3dcd11913f5079775d6598
MRIkerDev <iker.meza.devDev@gmail.com>
2025-02-05 22:05:45 -0700

    ya sirve

---
+++ src/main/java/com/whaletail/items/MjolnirItem.java
@@ -0,0 +1,4 @@
+package com.whaletail.items;
+
+public class MjolnirItem {
+}
--- src/main/java/com/whaletail/legendsofvalhalla/LegendsOfValhalla.java
+++ src/main/java/com/whaletail/legendsofvalhalla/LegendsOfValhalla.java
@@ -35,6 +35,8 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
+import software.bernie.geckolib.GeckoLib;
+import software.bernie.geckolib.animatable.GeoItem;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(LegendsOfValhalla.MODID)
